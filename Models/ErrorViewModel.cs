1cce0f23e7cdfc1f64430fd548c08b6fae60c2e7
plukic <petar.lukic@edu.fit.ba>
2017-12-16 12:29:05 +0100

    Initial project commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GradjevinskiDnevnik.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
