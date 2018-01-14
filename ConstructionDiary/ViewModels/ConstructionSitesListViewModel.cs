1bcd0f30863a4aa4d9e1f65b4f5d0b2429d5fe90
Kenan Selimović <kenan.selimovic@edu.fit.ba>
2018-01-14 11:19:30 +0100

    Implement construction sites filtering by city

--- ConstructionDiary/ViewModels/ConstructionSitesListViewModel.cs
+++ ConstructionDiary/ViewModels/ConstructionSitesListViewModel.cs
@@ -6,8 +6,12 @@ namespace ConstructionDiary.ViewModels
 
     public class ConstructionSitesListViewModel
     {
-        public List<ConstructionSite> ConstructionSites;
+        public List<ConstructionSite> ConstructionSites { get; set; }
 
-        public OpenStatus? OpenStatus;
+        public OpenStatus? OpenStatus { get; set; }
+
+        public List<City> Cities { get; set; }
+
+        public City SelectedCity { get; set; }
     }
 }
