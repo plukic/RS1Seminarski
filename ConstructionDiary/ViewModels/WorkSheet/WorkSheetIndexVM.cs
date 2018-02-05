c67d574aa06b9c9e5bcc5035f1c78945de8a4525
Petar Lukić <petar.lukic@mostar.evona.sk>
2018-02-05 15:59:08 +0100

    Worksheet control entry frontend validation

--- ConstructionDiary/ViewModels/WorkSheet/WorkSheetIndexVM.cs
+++ ConstructionDiary/ViewModels/WorkSheet/WorkSheetIndexVM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,11 @@ namespace ConstructionDiary.ViewModels.WorkSheet
     public class WorkSheetIndexVM
     {
         public int WorkSheetId { get; set; }
+        [DisplayName("Datum radnog lista")]
         public DateTime WorkSheetDate { get; set; }
+        [DisplayName("Gradilište")]
         public string ConstructionSiteName { get; set; }
+        [DisplayName("Stanje")]
         public bool IsLocked{ get; set; }
 
     }
